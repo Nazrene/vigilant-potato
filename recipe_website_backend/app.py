4b831f2a40087c462c1d0f995f79be6c54f45933
Naz <mercy.kamau@student.moringaschool.com>
2025-04-01 23:33:49 +0300

    Request Handling

--- recipe_website_backend/app.py
+++ recipe_website_backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask , request , jsonify
 
 app = Flask(__name__)
 
@@ -6,25 +6,34 @@ app = Flask(__name__)
 def home():
     return "Welcome to Only Pans!"
 
-@app.route("/recipes")
+@app.route("/recipes", methods=["GET" , "POST"])
 def recipes():
+    if request.method == "POST":
+        data = request.get_json()
+        return jsonify({"message": "Recipe received", "recipe": data}), 201
+    
     return "Here is a list of all recipes"
 
-@app.route("/recipe/<recipe_name>")
+@app.route("/recipe/<recipe_name>", methods=["GET"])
 def recipe(recipe_name):
-    return f"Showing details for{recipe_name} recipe"
+    return f"Showing details for {recipe_name} recipe"
 
-@app.route("/add-recipe")
+@app.route("/add-recipe", methods=["POST"])
 def new_recipe():
-    return f"Admin:Add a new recipe here!"
+    data = request.get_json
+    return jsonify({
+        "message": f"Recipe '{data['name']}' added suucessfully!" ,
+        "recipe": data
+    }) , 201
 
-@app.route("/edit-recipe/<recipe_name>")
+@app.route("/edit-recipe/<recipe_name>", methods=["PUT"])
 def edit_recipe(recipe_name):
-    return f"Admin:Updating {recipe_name} recipe"
+    data = request.json
+    return f"Admin:Updated {recipe_name} recipe to {data['name']}" , 200
 
-@app.route("/delete-recipe/<recipe_name>")
+@app.route("/delete-recipe/<recipe_name>", methods=["DELETE"])
 def delete_recipe(recipe_name):
-    return f"Admin:Deleting {recipe_name} recipe"
+    return f"Admin:Deleted {recipe_name} recipe" , 200
 
 if __name__ == "__main__":
     app.run(debug=True)
