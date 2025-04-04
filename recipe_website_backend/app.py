abcbd4c815d009e9af80a17fe97645471cac91ce
Naz <mercy.kamau@student.moringaschool.com>
2025-04-05 00:58:40 +0300

    Db created

--- recipe_website_backend/app.py
+++ recipe_website_backend/app.py
@@ -1,6 +1,12 @@
 from flask import Flask , request , jsonify
+from flask_sqlalchemy import SQLAlchemy
+from models import db
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///recipes.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
 recipes_list = [
     {"name": "spaghetti", "ingredients": ["pasta", "tomato sauce", "cheese"]},
---
+++ recipe_website_backend/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    instructions = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"<Recipe {self.name}>"
