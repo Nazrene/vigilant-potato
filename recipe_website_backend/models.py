08e796016bfd2b848ed1363736eda3042da2be1e
Naz <mercy.kamau@student.moringaschool.com>
2025-11-10 16:45:54 +0300

    ORM: User model

--- recipe_website_backend/models.py
+++ recipe_website_backend/models.py
@@ -2,6 +2,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    __tablename__= "users"
+
+    id = db.Column(db.Integer , primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(255) , nullable=False , unique=True, index=True)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    recipes = db.relationship("Recipe", back_populates="author", cascade="all, delete-orphan")
+    reviews = db.relationship("Review", back_populates="author", cascade="all, delete-orphan")
+
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
