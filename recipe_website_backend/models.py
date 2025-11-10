6087f2775b092f887a5d49f85ca7353f991ed7bf
Naz <mercy.kamau@student.moringaschool.com>
2025-11-10 17:25:38 +0300

    ORM:Recipe model

--- recipe_website_backend/models.py
+++ recipe_website_backend/models.py
@@ -15,9 +15,26 @@ class User(db.Model):
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False, index=True)
     ingredients = db.Column(db.Text, nullable=False)
     instructions = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.String(1024), nullable=True)
+    user_id = db.Column(db.Integer, db.Foreignkey("users.id"), nullable=False, index=True)
+
+    author = db.Relationship("User", back_populates="recipes")
+    reviews = db.Relationship("Review", back_populates="recipe", cascade="all, delete-orphan")
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.Text, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False, index=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipes.id"), nullable=False, index=True)
+
+    author = db.Relationship("User", back_populates="reviews")
+    recipe = db.Relationship("Recipe", back_populates="review")
+
 
     def __repr__(self):
         return f"<Recipe {self.name}>"
